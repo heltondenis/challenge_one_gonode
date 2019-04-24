4880e69fb3f8d10b29defda8414b8863141febc2
Helton Denis <heltondeniss@gmail.com>
2019-04-23 21:29:44 -0300

    Create router major age

--- index.js
+++ index.js
@@ -14,11 +14,27 @@ app.set('view engine', 'njk')
 
 const users = ['Helton', 'João', 'Igor']
 
+// Rota inicial que renderiza uma página com um formulário com um único campo age que representa a idade do usuário
 app.get('/', (req, res) => {
-  return res.render('njk', { users })
+  return res.render('home', { users })
 })
 
-app.get('/new', (req, res) => {
+// Checa se a idade do usuário é maior que 18 e o redireciona para a rota /major , caso contrário o redireciona para a rota /minor
+app.post('/check', (req, res) => {
+  if (req.body.age > 18) {
+    return res.redirect('/major' + '/' + req.body.age)
+  } else {
+    return res.redirect('/minor' + '/' + req.body.age)
+  }
+})
+
+// Rota que renderiza uma página com o texto: Você é maior de idade e possui x anos , onde x deve ser o valor informado no input do formulário
+app.get('/major/:age', (req, res) => {
+  return res.render('major', { age: req.params.age })
+})
+
+// Rota que renderiza uma página com o texto: Você é menor de idade e possui x anos , onde x deve ser o valor informado no input do formulário
+app.get('/minor', (req, res) => {
   return res.render('new')
 })
 
